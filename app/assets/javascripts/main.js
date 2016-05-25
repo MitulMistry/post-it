7686fe82b47781477f6a1aecff3774561f26d4bb
Mitul Mistry <mitulmistrydev@gmail.com>
2016-05-25 15:21:49 -0500

    working on js forms

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,51 @@
 'use strict';
+
+$(document).ready(function(){
+  attachListeners();
+  loadTags();
+  loadNotes();
+});
+
+function attachListeners(){
+
+    //create new tag
+    //createTag();
+  $('form#new_tag').submit(function(event) {
+    event.preventDefault(); //prevent form from submitting the default way
+
+    var values = $(this).serialize();
+    //console.log(values);
+
+    var posting = $.post('/tags', values);
+
+    posting.done(function(data) {
+      console.log(data);
+      //var post = data["post"];
+      //$("#tag_name").text(post["name"]);
+    });
+  });
+
+  $('form#new_note').submit(function(event) {
+    event.preventDefault(); //prevent form from submitting the default way
+
+    var values = $(this).serialize();
+    //console.log(values);
+
+    var posting = $.post('/notes', values);
+
+    posting.done(function(data) {
+      console.log(data);
+      //var post = data["post"];
+      //$("#tag_name").text(post["name"]);
+    });
+  });
+
+}
+
+function loadTags(){
+
+}
+
+function loadNotes(){
+  
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @note = Note.new
+    @tag = Tag.new
   end
 end
