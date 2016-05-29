fb7f86d4a3be972fd9983c38f482ef00d85cb93c
Mitul Mistry <mitulmistrydev@gmail.com>
2016-05-29 11:27:46 -0500

    formatter comments + clarification

--- app/assets/javascripts/formatter.js
+++ app/assets/javascripts/formatter.js
@@ -4,13 +4,14 @@ function Formatter(){ //formatter class
 
 Formatter.prototype.readableDate = function(dateTime){ //dateTime = 2016-05-26T16:24:19.457Z
   var date = dateTime.slice(0, 9).split('-'); //get first 10 characters, split into array based on dashes
-  return date[1] + '/' + date[2] + '/' + date[0] 
+  return date[1] + '/' + date[2] + '/' + date[0] //reorder with slashes - 05/26/2016
 }
 
 Formatter.prototype.titleCase = function(string){
-  string = string.toLowerCase().split(' ');
-  for (var i = 0; i < string.length; i++) {
-    string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+  var words = [];
+  words = string.toLowerCase().split(' '); //turn string into lowercase then split into array of words based on spaces
+  for (var i = 0; i < words.length; i++) { //loop through whole array of words
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1); //for each word, get the first character and set to uppercase, then add rest of the word with slice(1) - which gets second character to end of string
   }
-  return string.join(' ');
+  return words.join(' '); //join the array of words, add spaces, and return the new string
 }
